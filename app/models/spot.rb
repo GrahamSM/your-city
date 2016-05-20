19264897f004346f3bf72c16079f3303ff6c546a
Johsua Strub <jde.strub@gmail.com>
2016-05-20 00:31:08 -0400

    bunch of style changes

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,6 +4,6 @@ class Spot < ActiveRecord::Base
   belongs_to :category
   belongs_to :city
   validates :title, uniqueness: true, presence: true
-  validates :location, uniqueness: true, presence: true
+  validates :location, presence: true
   validates :description, presence: true
 end
